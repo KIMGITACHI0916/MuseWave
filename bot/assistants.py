2f1f824edaf89e728ede6524d3f7158b732453c6
KIMGITACHI0916 <148501410+KIMGITACHI0916@users.noreply.github.com>
2025-05-02 03:51:31 +0530

    Create assistants.py

---
+++ bot/assistants.py
@@ -0,0 +1,12 @@
+from pyrogram import Client
+
+API_ID = 20908204   # your api_id here
+API_HASH = "db17de0efb0565f536e63ea2eca3dbbe"
+SESSION = "BQE_CKwAXHI5QKy24SqXlNYsznn1QSRUsmwPnA3EPESIXxi8imVarKj0vygHHTNnZwekTbV89PdRWaHakv0wz8dzjt2WfryVTKI_GmgcKLyoOLKTbNflCSJPB_8a9QYJoJQwem7M4tPUJaRZIL4aY2XezFqdZ2UqN2VqQprN9-NNoRro6Ae0fab9qS8ZKyehA77sO3shjXgtFPaVisPUC6pxgm1-KEumymGjbPGWdl2Y93B2GJInVwCxbu9iupfcEwyeczwx5Qob_4W7cJgIbp6VswUs55LZHBIY9pJJ6X4LICfSaqez6yYxoD7e9PgxMHdw7pMZizkzKAj66BgR6QTVoc9bsQAAAAHZO-FLAA"
+
+assistant = Client(
+    name="assistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION
+)
