829402ec6580ba577888eab4d740d06f642a79a4
KIMGITACHI0916 <148501410+KIMGITACHI0916@users.noreply.github.com>
2025-05-02 04:42:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from handlers.play import play
 from handlers.control import add_to_queue, get_queue, get_current_track
 from handlers.control import pause, resume, skip, stop
 from config import BOT_TOKEN
-from bot.assistant import assistant  # adjust path if needed
+from assistant import assistant  # adjust path if needed
 from pyrogram import idle
 
 async def main():
